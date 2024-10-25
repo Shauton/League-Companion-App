3a44a1d4ba6b589019bc75748255db8895fab4a9
Shauton <clarkson.cameron913@gmail.com>
2024-10-25 13:11:34 -0400

    UI Change v1.0.0

--- app/src/main/java/com/example/leaguecompanionapp/ui/home/HomeViewModel.java
+++ app/src/main/java/com/example/leaguecompanionapp/ui/home/HomeViewModel.java
@@ -10,7 +10,7 @@ public class HomeViewModel extends ViewModel {
 
     public HomeViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
+        mText.setValue("Sign Up");
     }
 
     public LiveData<String> getText() {
